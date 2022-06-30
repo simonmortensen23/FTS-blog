9fbe7b2eb1336f1d275c331195eb137386f52ba8
simonmortensen23 <simon020393@gmail.com>
2022-06-30 13:17:41 +0000

    deploy fix

--- codestar/settings.py
+++ codestar/settings.py
@@ -162,9 +162,9 @@ MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CLOUDINARY_STORAGE = {
-'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 
 # Default primary key field type
